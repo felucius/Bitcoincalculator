11f6c92ff859395f3328a63f5751bf1edc4898e1
Maxime <maximedelange1991@hotmail.com>
2017-09-02 19:13:55 +0200

    Added basic Android project with two empty domain classes

---
+++ BitcoinCalculator_App/app/src/main/java/maximedelange/bitcoincalculator/Domain/APICalls.java
@@ -0,0 +1,9 @@
+package maximedelange.bitcoincalculator.Domain;
+
+/**
+ * Created by M on 9/2/2017.
+ */
+
+public class APICalls {
+
+}
---
+++ BitcoinCalculator_App/app/src/main/java/maximedelange/bitcoincalculator/Domain/Bitcoin.java
@@ -0,0 +1,8 @@
+package maximedelange.bitcoincalculator.Domain;
+
+/**
+ * Created by M on 9/2/2017.
+ */
+
+public class Bitcoin {
+}

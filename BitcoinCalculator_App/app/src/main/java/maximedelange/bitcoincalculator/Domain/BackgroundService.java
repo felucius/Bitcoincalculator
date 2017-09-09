e205cceab2d6797129e8857532b7a75391d35a86
Maxime <maximedelange1991@hotmail.com>
2017-09-09 16:25:30 +0200

    Service shows a repetetive message when bitcoin price falls down a certain amount

--- BitcoinCalculator_App/app/src/main/java/maximedelange/bitcoincalculator/Domain/BackgroundService.java
+++ BitcoinCalculator_App/app/src/main/java/maximedelange/bitcoincalculator/Domain/BackgroundService.java
@@ -3,7 +3,9 @@ package maximedelange.bitcoincalculator.Domain;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.widget.Toast;
 
@@ -35,18 +37,27 @@ public class BackgroundService extends Service {
 
         // Running timer to show a message to the user repetetive.
         Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                if(bitcoin > 2000){
-                    System.out.println("Value higher than 2000");
-                    Toast.makeText(context, bitcoinValue, Toast.LENGTH_SHORT).show();
-                }else{
-                    System.out.println("Value lower than 2000");
-                    Toast.makeText(context, "Bitcoin value is lower than 2000", Toast.LENGTH_SHORT).show();
+        timer.scheduleAtFixedRate( new TimerTask() {
+            private Handler updateUI = new Handler(){
+                @Override
+                public void dispatchMessage(Message msg) {
+                    super.dispatchMessage(msg);
+                    // Checking if bitcoin value is higher than a certain amount.
+                    if(bitcoin > 2000){
+                        System.out.println("Value higher than 2000");
+                        Toast.makeText(context, bitcoinValue, Toast.LENGTH_SHORT).show();
+                    }else{
+                        System.out.println("Value lower than 2000");
+                        Toast.makeText(context, "Bitcoin value is lower than 2000", Toast.LENGTH_SHORT).show();
+                    }
                 }
+            };
+            public void run() {
+                try {
+                    updateUI.sendEmptyMessage(0);
+                } catch (Exception e) {e.printStackTrace(); }
             }
-        },0, 5000);
+        }, 0, 5000); // Start inmmediatly and refresh the message every 5 seconds.
 
         // Returning sticky is making the service run constantly without stopping, unless told explicitly
         return START_STICKY;
